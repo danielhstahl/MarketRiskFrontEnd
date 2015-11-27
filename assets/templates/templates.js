fed8cc5729b89958c1c90936dd5263b96cc7a838
phillyfan1138 <danstahl1138@gmail.com>
2015-11-27 14:34:37 -0500

    cleaned up

--- assets/templates/templates.js
+++ assets/templates/templates.js
@@ -3,7 +3,24 @@
 templates['MarketRiskParameters'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     return "<div class=\"input-group\">\n  <span class=\"input-group-addon\" id=\"simulation\">Number of simulations</span>\n  <input type=\"text\" class=\"form-control\" placeholder=\"Username\" aria-describedby=\"basic-addon1\">\n</div>\n";
 },"useData":true});
+templates['largetext'] = template({"1":function(container,depth0,helpers,partials,data) {
+    var helper, alias1=container.escapeExpression;
+
+  return "<h2> "
+    + alias1(((helper = (helper = helpers.key || (data && data.key)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"key","hash":{},"data":data}) : helper)))
+    + ": "
+    + alias1(container.lambda(depth0, depth0))
+    + "<h2>\n";
+},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return ((stack1 = helpers.each.call(depth0 != null ? depth0 : {},(depth0 != null ? depth0.data : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "");
+},"useData":true});
 templates['progressbar'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<div class=\"progress\">\n  <div class=\"progress-bar progress-bar-striped active\" role=\"progressbar\" aria-valuenow=\"100\" aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"width: 100%\">\n    <!--<span class=\"sr-only\">45% Complete</span>-->\n  </div>\n</div>\n";
+    var helper;
+
+  return "<div class=\"progress\">\n  <div class=\"progress-bar progress-bar-striped active\" role=\"progressbar\" id='progressBar' aria-valuenow=\"100\" aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"width: "
+    + container.escapeExpression(((helper = (helper = helpers.percent || (depth0 != null ? depth0.percent : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"percent","hash":{},"data":data}) : helper)))
+    + "%\">\n    <!--<span class=\"sr-only\">45% Complete</span>-->\n  </div>\n</div>\n";
 },"useData":true});
 })();
--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 var child_process = require('child_process');
-var https=require('https');
 var http=require('http');
 var express = require('express');
 var bodyParser=require('body-parser');
 var exphbs = require('express-handlebars');
-
+var handleYield=require('handleYield');
 var app = express();
 var server = http.createServer(app).listen(8000); //required fro socket.io
 var io = require('socket.io').listen(server);
@@ -25,8 +24,6 @@ app.get('/', function (req, res) {
 });
 io.on('connection', function(socket) {
   var child = child_process.spawn('/home/daniel/Documents/cpp/marketRisk/./marketRisk',
-  //child = child_process.spawn('./test',
-    //["marketRisk"], //parameters passed to the market risk CPP program.
     {
       stdio: [
         'pipe', //pipe parent to child
@@ -35,275 +32,48 @@ io.on('connection', function(socket) {
       ]
     }
   );
+  child.stdout.on('data', function (data) {
+    io.emit('data', data.toString('utf8'));
+  });
+  child.stderr.on('data', function (data) {
+    console.log('stderr: ' + data);
+  });
+  child.on('close', function (code) {
+    console.log('child process exited with code ' + code);
+  });
   socket.on('getYield', function(data) { //if "submit" is clicked ona  project page
     //retreiveFutures(child);
-    retreiveLiborAndSwap(child);
-    https.get("https://api.stlouisfed.org/fred/series/observations?series_id=USD1WKD156N&api_key=6b75e4bc06a6ed991a7a9cc64d70c3fa&file_type=json", function(result){callback(result, child);}); //1 week libor history
+    var yields=new handleYield(child);
+    yields.retreiveLiborAndSwap(child);
+
   });
   socket.on('getMC', function(data) {
-    getPortfolio(3000);
+    var portfolio=getPortfolio(3000);
     child.stdin.write(JSON.stringify(portfolio));
     child.stdin.write("\n");
-    child.stdout.on('data', function (data) {
-      io.emit('mc', ''+data);
-    });
-    child.stderr.on('data', function (data) {
-      console.log('stderr: ' + data);
-    });
-    child.on('close', function (code) {
-      console.log('child process exited with code ' + code);
-    });
   });
 });
 
 
 
-var portfolio=[];
-var YieldCurveUnExtrapolated={};
-var historicalResults="";
-var retreivedHistoricalResults=false;
-var retreivedYieldResults=false;
-var retreivedPortfolio=false;
-var j={"j":0};
-
-function retreiveLiborAndSwap(child){
-  var apiStrings=[
-    {
-      description:"Most Current 1 week LIBOR",
-      url:"https://api.stlouisfed.org/fred/series/observations?series_id=USD1WKD156N&api_key=6b75e4bc06a6ed991a7a9cc64d70c3fa&file_type=json&sort_order=desc&limit=1",
-      daysPlus:7,//7 day convention
-      type:"LIBOR"
-    },
-    {
-      description:"Most Current 1 month LIBOR",
-      url:"https://api.stlouisfed.org/fred/series/observations?series_id=USD1MTD156N&api_key=6b75e4bc06a6ed991a7a9cc64d70c3fa&file_type=json&sort_order=desc&limit=1",
-      daysPlus:30,//thirty day convention
-      type:"LIBOR"
-    },
-    {
-      description:"Most Current 3 month LIBOR",
-      url:"https://api.stlouisfed.org/fred/series/observations?series_id=USD3MTD156N&api_key=6b75e4bc06a6ed991a7a9cc64d70c3fa&file_type=json&sort_order=desc&limit=1",
-      daysPlus:90,
-      type:"LIBOR"
-    },
-    {
-      description:"Most Current 6 month LIBOR",
-      url:"https://api.stlouisfed.org/fred/series/observations?series_id=USD6MTD156N&api_key=6b75e4bc06a6ed991a7a9cc64d70c3fa&file_type=json&sort_order=desc&limit=1",
-      daysPlus:180,
-      type:"LIBOR"
-    },
-    {
-      description:"Most Current 12 month LIBOR",
-      url:"https://api.stlouisfed.org/fred/series/observations?series_id=USD12MD156N&api_key=6b75e4bc06a6ed991a7a9cc64d70c3fa&file_type=json&sort_order=desc&limit=1",
-      daysPlus:360,
-      type:"LIBOR"
-    },
-    {
-      description:"Most Current 1 year swap on 3 month LIBOR",
-      url:"https://api.stlouisfed.org/fred/series/observations?series_id=DSWP1&api_key=6b75e4bc06a6ed991a7a9cc64d70c3fa&file_type=json&sort_order=desc&limit=1",
-      daysPlus:360,
-      type:"Swap"
-    },
-    {
-      description:"Most Current 2 year swap on 3 month LIBOR",
-      url:"https://api.stlouisfed.org/fred/series/observations?series_id=DSWP2&api_key=6b75e4bc06a6ed991a7a9cc64d70c3fa&file_type=json&sort_order=desc&limit=1",
-      daysPlus:720,
-      type:"Swap"
-    },
-    {
-      description:"Most Current 3 year swap on 3 month LIBOR",
-      url:"https://api.stlouisfed.org/fred/series/observations?series_id=DSWP3&api_key=6b75e4bc06a6ed991a7a9cc64d70c3fa&file_type=json&sort_order=desc&limit=1",
-      daysPlus:1080,
-      type:"Swap"
-    },
-    {
-      description:"Most Current 4 year swap on 3 month LIBOR",
-      url:"https://api.stlouisfed.org/fred/series/observations?series_id=DSWP4&api_key=6b75e4bc06a6ed991a7a9cc64d70c3fa&file_type=json&sort_order=desc&limit=1",
-      daysPlus:1440,
-      type:"Swap"
-    },
-    {
-      description:"Most Current 5 year swap on 3 month LIBOR",
-      url:"https://api.stlouisfed.org/fred/series/observations?series_id=DSWP5&api_key=6b75e4bc06a6ed991a7a9cc64d70c3fa&file_type=json&sort_order=desc&limit=1",
-      daysPlus:1800,
-      type:"Swap"
-    },
-    {
-      description:"Most Current 7 year swap on 3 month LIBOR",
-      url:"https://api.stlouisfed.org/fred/series/observations?series_id=DSWP7&api_key=6b75e4bc06a6ed991a7a9cc64d70c3fa&file_type=json&sort_order=desc&limit=1",
-      daysPlus:2520,
-      type:"Swap"
-    },
-    {
-      description:"Most Current 10 year swap on 3 month LIBOR",
-      url:"https://api.stlouisfed.org/fred/series/observations?series_id=DSWP10&api_key=6b75e4bc06a6ed991a7a9cc64d70c3fa&file_type=json&sort_order=desc&limit=1",
-      daysPlus:3600,
-      type:"Swap"
-    },
-    {
-      description:"Most Current 30 year swap on 3 month LIBOR",
-      url:"https://api.stlouisfed.org/fred/series/observations?series_id=DSWP30&api_key=6b75e4bc06a6ed991a7a9cc64d70c3fa&file_type=json&sort_order=desc&limit=1",
-      daysPlus:10800,
-      type:"Swap"
-    }
-  ];
-  YieldCurveUnExtrapolated=Array(apiStrings.length);
-  runFunc(apiStrings, 0, child);
-}
-
-function retreiveFutures(child){
-  var getData;
-  YieldCurveUnExtrapolated=[];
-  http.get("http://www.cmegroup.com/CmeWS/mvc/Quotes/Future/1/G", function(response) {
-    var str = '';
-    //another chunk of data has been recieved, so append it to `str`
-    response.on('data', function (chunk) {
-      str += chunk;
-    });
-    //the whole response has been recieved
-    response.on('end', function () {
-      var oneDay = 24*60*60*1000;
-      getData=JSON.parse(str);
-
-      getData=getData.quotes;
-      var Wednesday=3;
-      var twoWeeks=13;
-      var n=getData.length;
-      var currdate=new Date();
-      for(var i=0; i<n; i++){
-        if(getData[i].last!=='-'){
-          var year="";
-          var month="";
-          var date="";
-          for(var j=0; j<4;j++){
-            year+=getData[i].expirationDate[j];
-          }
-          date+=year+"-";
-          month+=getData[i].expirationDate[4];
-          month+=getData[i].expirationDate[5];
-          //day="01";
-          var myDate=new Date(year, month-1, 1);
-          var beginningDay=myDate.getDay();
-
-          //var daysAdd=beginningDay<=3?(3-beginningDay+14):7-(beginningDay-3)+14; //wednesday is "3"
-          var daysAdd=beginningDay<=Wednesday?(Wednesday-beginningDay+twoWeeks):7-(beginningDay-Wednesday)+twoWeeks; //wednesday is "3"
-          date+=month+"-"+daysAdd;
-          //date+=getData[i].expirationDate[6];
-          //date+=getData[i].expirationDate[7];
-
-          YieldCurveUnExtrapolated.push({
-            value:getData[i].last,
-            date:date,
-            daysPlus:Math.round(Math.abs((myDate.getTime() - currdate.getTime())/(oneDay))),
-            type:"Futures"
-          });
-        }
-
-      }
+//var portfolio=[];
+//var YieldCurveUnExtrapolated={};
+//var historicalResults="";
+//var retreivedHistoricalResults=false;
+//var retreivedYieldResults=false;
+//var retreivedPortfolio=false;
+//var j={"j":0};
 
 
-      if(!YieldCurveUnExtrapolated[0]){
-        for(var i=0; i<n; i++){
-          if(getData[i].priorSettle!=='-'){
-            var year="";
-            var month="";
-            var date="";
-            for(var j=0; j<4;j++){
-              year+=getData[i].expirationDate[j];
-            }
-            date+=year+"-";
-            month+=getData[i].expirationDate[4];
-            month+=getData[i].expirationDate[5];
-            //day="01";
-            var myDate=new Date(year, month-1, 1);
-            var beginningDay=myDate.getDay();
-
-            //var daysAdd=beginningDay<=3?(3-beginningDay+14):7-(beginningDay-3)+14; //wednesday is "3"
-            var daysAdd=beginningDay<=Wednesday?(Wednesday-beginningDay+twoWeeks):7-(beginningDay-Wednesday)+twoWeeks; //wednesday is "3"
-            date+=month+"-"+daysAdd;
-            //date+=getData[i].expirationDate[6];
-            //date+=getData[i].expirationDate[7];
-
-            YieldCurveUnExtrapolated.push({
-              value:getData[i].priorSettle,
-              date:date,
-              daysPlus:Math.round(Math.abs((myDate.getTime() - currdate.getTime())/(oneDay))),
-              type:"Futures"
-            });
-          }
-        }
-      }
-      retreivedYieldResults=true;
-      sendDataToCPP(child);
-    });
-  });
-}
 
 
-var callback = function(response, child) {
-  var str = '';
-  //another chunk of data has been recieved, so append it to `str`
-  response.on('data', function (chunk) {
-    historicalResults += chunk;
-  });
-  //the whole response has been recieved
-  response.on('end', function () {
-    retreivedHistoricalResults=true;
-    sendDataToCPP(child);
-  });
-}
-var clb=function(response, i, n, jObj, desc, daysPlus, type, child){
-  var str = '';
-  //another chunk of data has been recieved, so append it to `str`
-  response.on('data', function (chunk) {
-    str += chunk;
-  });
-  //the whole response has been recieved, so we just print it out here
-  response.on('end', function () {
-    var obj=JSON.parse(str);
 
-    YieldCurveUnExtrapolated[i]={date:obj.observations[0].date, value:obj.observations[0].value, description:desc, daysPlus:daysPlus, type:type};
-    jObj.j++;
-    //console.log(jObj.j);
-    if(n==jObj.j){
-      retreivedYieldResults=true;
-      //console.log(YieldCurveUnExtrapolated);
-      sendDataToCPP(child);
-    }
-  });
-}
-function sendDataToCPP(child){
-  if(retreivedYieldResults&&retreivedHistoricalResults){
 
-    child.stdin.write(JSON.stringify(YieldCurveUnExtrapolated));
-    child.stdin.write("\n");
-    child.stdin.write(historicalResults);
-    child.stdin.write("\n");
-    //child.stdin.write(JSON.stringify(portfolio));
-    //child.stdin.write("\n");
-    child.stdout.on('data', function (data) {
-      //console.log(''+data);
-      io.emit('yield', ''+data);
-    });
-    child.stderr.on('data', function (data) {
-      console.log('stderr: ' + data);
-    });
-    child.on('close', function (code) {
-      console.log('child process exited with code ' + code);
-    });
-  }
-}
-var runFunc=function(arrayUrl, i, child){
-  var n=arrayUrl.length;
-  if(i<n){
-    https.get(arrayUrl[i].url, function(response){
-      clb(response, i, n, j, arrayUrl[i].description, arrayUrl[i].daysPlus, arrayUrl[i].type, child);
-    });
-    runFunc(arrayUrl, i+1, child);
-  }
-}
+
+
 var getPortfolio=function(numAssets){
   var currdate=new Date();
+  var portfolio=[];
   Date.prototype.addDays = function(days){
       var dat = new Date(this.valueOf());
       dat.setDate(dat.getDate() + days);
@@ -328,5 +98,6 @@ var getPortfolio=function(numAssets){
   for(var i=0; i<numAssets/4; i++){ //create a fake portfolio
     portfolio.push({maturity:currdate.addDays(i+1).yyyymmdd(), strike:.99, underlyingMaturity:currdate.addDays(i+91).yyyymmdd(), type:"call"});
   }
+  return portfolio;
 //  retreivedPortfolio=true;
 }
